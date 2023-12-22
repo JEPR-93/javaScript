2c8c59ab886fe3b3cb0596ea719933df5b9dd9f0
JEPR-93 <jepr93@hotmail.com>
2023-12-21 23:14:12 -0300

    array

--- script.js
+++ script.js
@@ -1,90 +1,106 @@
-  // Lista de palabras para el juego
-  const palabras = ["javascript", "html", "css", "python", "react", "angular", "nodejs"];
+// Lista de palabras para el juego
+const palabras = ["javascript", "html", "css", "python", "react", "angular", "nodejs"];
 
-  // Seleccionar una palabra aleatoria
-  let palabraSecreta = palabras[Math.floor(Math.random() * palabras.length)];
+// Array de búsqueda adicional
+const palabrasAdicionales = ["java", "php", "typescript", "mongodb", "redux"];
 
-  // Inicializar el estado del juego
-  let palabraAdivinada = Array(palabraSecreta.length).fill("_");
-  let intentosRestantes = 6;
-  let letrasUsadas = [];
+// Objeto de búsqueda adicional
+const objetosBusqueda = {
+    lenguajes: ["java", "php", "typescript"],
+    frameworks: ["react", "angular"],
+    basesDeDatos: ["mongodb", "mysql"]
+};
 
-  // Función para mostrar la palabra actualizada en la interfaz
-  function mostrarPalabra() {
-      document.getElementById('palabra').innerText = palabraAdivinada.join(' ');
-  }
+// Seleccionar una palabra aleatoria
+let palabraSecreta = obtenerPalabraAleatoria();
 
-  // Función para mostrar los intentos restantes
-  function mostrarIntentos() {
-      document.getElementById('intentos').innerText = `Intentos restantes: ${intentosRestantes}`;
-  }
+// Inicializar el estado del juego
+let palabraAdivinada = Array(palabraSecreta.length).fill("_");
+let intentosRestantes = 6;
+let letrasUsadas = [];
 
-  // Función para mostrar las letras usadas
-  function mostrarLetrasUsadas() {
-      document.getElementById('letras-usadas').innerText = `Letras usadas: ${letrasUsadas.join(', ')}`;
-  }
+// Función para obtener una palabra aleatoria de los arrays
+function obtenerPalabraAleatoria() {
+    const todasLasPalabras = palabras.concat(palabrasAdicionales);
+    return todasLasPalabras[Math.floor(Math.random() * todasLasPalabras.length)];
+}
 
-  // Función para actualizar la imagen del ahorcado
-  function actualizarImagenAhorcado() {
-      const imagenAhorcado = document.getElementById('imagen-ahorcado');
-      imagenAhorcado.src = `/asset/img/ahorcado.png-${6 - intentosRestantes}.png`;
-  }
+// Función para mostrar la palabra actualizada en la interfaz
+function mostrarPalabra() {
+    document.getElementById('palabra').innerText = palabraAdivinada.join(' ');
+}
 
-  // Función para manejar la entrada del usuario
-  function manejarEntrada(letra) {
-      if (letrasUsadas.includes(letra)) {
-          alert('Ya has utilizado esa letra. Elige otra.');
-          return;
-      }
+// Función para mostrar los intentos restantes
+function mostrarIntentos() {
+    document.getElementById('intentos').innerText = `Intentos restantes: ${intentosRestantes}`;
+}
 
-      letrasUsadas.push(letra);
+// Función para mostrar las letras usadas
+function mostrarLetrasUsadas() {
+    document.getElementById('letras-usadas').innerText = `Letras usadas: ${letrasUsadas.join(', ')}`;
+}
 
-      if (palabraSecreta.includes(letra)) {
-          for (let i = 0; i < palabraSecreta.length; i++) {
-              if (palabraSecreta[i] === letra) {
-                  palabraAdivinada[i] = letra;
-              }
-          }
-      } else {
-          intentosRestantes--;
-      }
+// Función para actualizar la imagen del ahorcado
+function actualizarImagenAhorcado() {
+    const imagenAhorcado = document.getElementById('imagen-ahorcado');
+    imagenAhorcado.src = `/asset/img/ahorcado.png-${6 - intentosRestantes}.png`;
+}
 
-      mostrarPalabra();
-      mostrarIntentos();
-      mostrarLetrasUsadas();
-      actualizarImagenAhorcado();
+// Función para manejar la entrada del usuario
+function manejarEntrada(letra) {
+    if (letrasUsadas.includes(letra)) {
+        alert('Ya has utilizado esa letra. Elige otra.');
+        return;
+    }
 
-      if (intentosRestantes === 0) {
-          alert(`¡Perdiste! La palabra era "${palabraSecreta}".`);
-          reiniciarJuego();
-      } else if (!palabraAdivinada.includes("_")) {
-          alert("¡Felicidades! ¡Has adivinado la palabra!");
-          reiniciarJuego();
-      }
-  }
+    letrasUsadas.push(letra);
 
-  // Función para reiniciar el juego
-  function reiniciarJuego() {
-      palabraSecreta = palabras[Math.floor(Math.random() * palabras.length)];
-      palabraAdivinada = Array(palabraSecreta.length).fill("_");
-      intentosRestantes = 6;
-      letrasUsadas = [];
-      mostrarPalabra();
-      mostrarIntentos();
-      mostrarLetrasUsadas();
-      actualizarImagenAhorcado();
-  }
+    if (palabraSecreta.includes(letra)) {
+        for (let i = 0; i < palabraSecreta.length; i++) {
+            if (palabraSecreta[i] === letra) {
+                palabraAdivinada[i] = letra;
+            }
+        }
+    } else {
+        intentosRestantes--;
+    }
 
-  // Inicializar el juego al cargar la página
-  mostrarPalabra();
-  mostrarIntentos();
-  mostrarLetrasUsadas();
-  actualizarImagenAhorcado();
+    mostrarPalabra();
+    mostrarIntentos();
+    mostrarLetrasUsadas();
+    actualizarImagenAhorcado();
 
-  // Evento para manejar la entrada del usuario
-  document.addEventListener('keydown', function (event) {
-      const letra = event.key.toLowerCase();
-      if (letra.match(/[a-z]/) && letra.length === 1) {
-          manejarEntrada(letra);
-      }
-  });
+    if (intentosRestantes === 0) {
+        alert(`¡Perdiste! La palabra era "${palabraSecreta}".`);
+        reiniciarJuego();
+    } else if (!palabraAdivinada.includes("_")) {
+        alert("¡Felicidades! ¡Has adivinado la palabra!");
+        reiniciarJuego();
+    }
+}
+
+// Función para reiniciar el juego
+function reiniciarJuego() {
+    palabraSecreta = obtenerPalabraAleatoria();
+    palabraAdivinada = Array(palabraSecreta.length).fill("_");
+    intentosRestantes = 6;
+    letrasUsadas = [];
+    mostrarPalabra();
+    mostrarIntentos();
+    mostrarLetrasUsadas();
+    actualizarImagenAhorcado();
+}
+
+// Inicializar el juego al cargar la página
+mostrarPalabra();
+mostrarIntentos();
+mostrarLetrasUsadas();
+actualizarImagenAhorcado();
+
+// Evento para manejar la entrada del usuario
+document.addEventListener('keydown', function (event) {
+    const letra = event.key.toLowerCase();
+    if (letra.match(/[a-z]/) && letra.length === 1) {
+        manejarEntrada(letra);
+    }
+});
